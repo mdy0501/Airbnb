1987ca0cbf116f840a02363acc304004158d8cb4
JunHee Lee <jhlee910609@gmail.com>
2017-08-06 14:46:09 +0900

    중복 라이브러리 삭

---
+++ Airbnb/app/src/main/java/com/android/airbnb/MakeWishListActivity.java
@@ -0,0 +1,15 @@
+package com.android.airbnb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeWishListActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_wish_list);
+    }
+}
