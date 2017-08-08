3337499d2c6b510619d4a9f98ceafa97081a63d7
Daniel Min <dongyeon89@gmail.com>
2017-08-08 21:53:39 +0900

    로그아웃 Post 코드 작성

--- Airbnb/app/src/main/java/com/android/airbnb/data/ApiService.java
+++ Airbnb/app/src/main/java/com/android/airbnb/data/ApiService.java
@@ -6,6 +6,7 @@ import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -33,7 +34,13 @@ public interface ApiService {
     @Multipart
     @POST("apis/user/login/")
     Call<LoginResult> postLoginData(@Part("email") RequestBody email,
-                                  @Part("password") RequestBody password);
+                                    @Part("password") RequestBody password);
+
+    // Key : Authorization
+    // Value : Token 762e7a853bbf466be109b91f2249b80411ebb99a
+
+    @GET("apis/user/logout/")
+    Call<ResponseBody> getLogout(@Header("Authorization") String token);
 
     @GET("apis/house/")
     Call<List<RoomsData>> getTotalHouse();
