88ffd9cd2f6f4be7a6d714db5b5c5d9fd426b3d4
Daniel Min <dongyeon89@gmail.com>
2017-08-09 23:18:02 +0900

    PreferenceUtil 작성 (로그인 시, token값 저장을 위해서)

---
+++ Airbnb/app/src/main/java/com/android/airbnb/util/PreferenceUtil.java
@@ -0,0 +1,37 @@
+package com.android.airbnb.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by MDY on 2017-08-09.
+ */
+
+public class PreferenceUtil {
+    private static SharedPreferences sharedPreferences = null;
+
+    private static void setSharedPreferences(Context context){
+        if(sharedPreferences == null)
+            sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE); // MODE_PRIVATE - 다른 앱이 접근하지 못하게
+    }
+
+    private static void setString(Context context, String key, String value){
+        setSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    private static String getString(Context context, String key){
+        setSharedPreferences(context);
+        return sharedPreferences.getString(key, "해당 데이터 없음");
+    }
+
+    // token
+    public static void setToken(Context context, String token){
+        setString(context,"userToken", token);
+    }
+    public static String getToken(Context context) {
+        return getString(context, "userToken");
+    }
+}
