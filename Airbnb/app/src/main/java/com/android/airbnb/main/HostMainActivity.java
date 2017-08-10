1dd1ae2aa37f33598e58b9945b5c8e4ce9e4f636
Daniel Min <dongyeon89@gmail.com>
2017-08-10 18:11:30 +0900

    Host 관련 Activity 및 Fragment 생성 - HostMainActivity - HostMessageFragment - HostCalendarFragment - HostRoomsFragment - HostStatisticsFragment - HostProfileFragment

---
+++ Airbnb/app/src/main/java/com/android/airbnb/main/HostMainActivity.java
@@ -0,0 +1,17 @@
+package com.android.airbnb.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.airbnb.R;
+
+public class HostMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_main);
+
+
+    }
+}
