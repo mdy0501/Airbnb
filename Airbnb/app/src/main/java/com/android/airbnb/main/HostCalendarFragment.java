1cc40b0520788b6bc723e148ad800e4e5d366ab3
Daniel Min <dongyeon89@gmail.com>
2017-08-10 23:28:24 +0900

    Host - Calendar화면 뷰 작성

--- Airbnb/app/src/main/java/com/android/airbnb/main/HostCalendarFragment.java
+++ Airbnb/app/src/main/java/com/android/airbnb/main/HostCalendarFragment.java
@@ -1,11 +1,13 @@
 package com.android.airbnb.main;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.android.airbnb.R;
 
@@ -14,17 +16,30 @@ import com.android.airbnb.R;
  */
 public class HostCalendarFragment extends Fragment {
 
+    private HostMainActivity hostMainActivity;
+    private TextView txtTitle, txtDescription;
 
     public HostCalendarFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        hostMainActivity = (HostMainActivity) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_host_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_host_calendar, container, false);
+        setViews(view);
+        return view;
+    }
+
+    private void setViews(View view){
+        txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+        txtDescription = (TextView) view.findViewById(R.id.txtDescription);
     }
 
 }
