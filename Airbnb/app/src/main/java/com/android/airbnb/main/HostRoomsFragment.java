06c2e7880894e3b7ebe603ff14daa186b23c587c
Daniel Min <dongyeon89@gmail.com>
2017-08-10 23:28:40 +0900

    Host - Calendar화면 뷰 작성

--- Airbnb/app/src/main/java/com/android/airbnb/main/HostRoomsFragment.java
+++ Airbnb/app/src/main/java/com/android/airbnb/main/HostRoomsFragment.java
@@ -1,30 +1,63 @@
 package com.android.airbnb.main;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.airbnb.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HostRoomsFragment extends Fragment {
+public class HostRoomsFragment extends Fragment implements View.OnClickListener{
+
+    private HostMainActivity hostMainActivity;
+    private TextView txtTitle, txtDescription;
+    private ImageButton imgBtnAddRooms;
 
 
     public HostRoomsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        hostMainActivity = (HostMainActivity) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_host_rooms, container, false);
+        View view = inflater.inflate(R.layout.fragment_host_rooms, container, false);
+        setViews(view);
+        setListeners();
+        return view;
     }
 
+    private void setViews(View view){
+        txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+        txtDescription = (TextView) view.findViewById(R.id.txtDescription);
+        imgBtnAddRooms = (ImageButton) view.findViewById(R.id.imgBtnAddRooms);
+    }
+
+    private void setListeners(){
+        imgBtnAddRooms.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgBtnAddRooms:
+                Toast.makeText(hostMainActivity, "숙소 추가버튼 클릭", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
