d48fd6a25b5238af42e862a651e8865a045873c0
JunHee Lee <jhlee910609@gmail.com>
2017-08-14 21:34:21 +0900

    OnCalendarChangedListener implements, GridAdapter 생성 및 set 되는 구간을 onCreateViewHolder로 이동

--- Airbnb/app/src/main/java/com/android/airbnb/calendar/CustomCalendarAdapter.java
+++ Airbnb/app/src/main/java/com/android/airbnb/calendar/CustomCalendarAdapter.java
@@ -2,6 +2,7 @@ package com.android.airbnb.calendar;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,17 +11,26 @@ import android.widget.TextView;
 
 import com.android.airbnb.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by JunHee on 2017. 8. 10..
  */
 
-public class CustomCalendarAdapter extends RecyclerView.Adapter<CustomCalendarAdapter.Holder> {
+public class CustomCalendarAdapter extends RecyclerView.Adapter<CustomCalendarAdapter.Holder> implements GridAdapter.OnCalendarChangedListener {
 
     private List<CalendarData> dateList;
     private Context mContext;
     private GridAdapter.OnTextChangedListener mOnTextChangedListener;
+    public List<GridAdapter> adapters = new ArrayList<>();
+    private int position = 0;
+    GridAdapter gridAdapter = null;
+
+    public final static String STATUS_CHECKIN = "status_check_in";
+    public final static String STATUS_CHECKOUT = "status_check_out";
+    public final static String STATUS_NOTHING = "status_check_nothing";
+    public static String status = STATUS_CHECKIN;
 
     public CustomCalendarAdapter(List<CalendarData> dateList, GridAdapter.OnTextChangedListener mOnTextChangedListener, Context mContext) {
         this.mOnTextChangedListener = mOnTextChangedListener;
@@ -28,21 +38,51 @@ public class CustomCalendarAdapter extends RecyclerView.Adapter<CustomCalendarAd
         this.mContext = mContext;
     }
 
+    /**
+     * 카운트(일종의 flag) 값을 만들어서 position을 돌 때마다 position 값이 증가되어 onCreateViewHolder를 통해 증가된 인덱스 값의 데이터를 꺼내어 데이터를 셋팅 후, holder 생성
+     */
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.calendar_item, parent, false);
-        return new Holder(view);
+        // 홀더 셋팅 후, 리턴하게 로직을 만듬
+        Holder holder = new Holder(view);
+        holder.setCalendarMonth(dateList.get(position).getMonth() + "월");
+        gridAdapter = new GridAdapter(dateList.get(position), (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE), mOnTextChangedListener, this);
+        adapters.add(gridAdapter);
+        holder.getCalendarGridView().setAdapter(gridAdapter);
+        position++;
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(Holder holder, int position) {
-        CalendarData calendarData = dateList.get(position);
-        List<Integer> days = calendarData.getDays();
-        holder.setCalendarMonth(calendarData.getMonth() + "월");
+    public void resetAll() {
+        Log.d("CustomCalendar", "adapter size === " + adapters.size());
+        for (GridAdapter adapter : adapters) {
+            adapter.resetAll();
+        }
+        Log.d("CustomCalendar", "checkInChanged === done ");
+    }
 
-        // holder가 gridView를 품고 있기 때문에, 이곳에서 setAdapter를 해준다.
-        GridAdapter gridAdapter = new GridAdapter(days, (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE), mOnTextChangedListener);
-        holder.getCalendarGridView().setAdapter(gridAdapter);
+    // TODO 완성시키기
+    private void findCheckIn() {
+//        for(GridAdapter adapter : adapters){
+//            if (adapter.getStaus() == STATUS_CHECKOUT){
+//                adapter.get
+//            }
+//        }
+    }
+
+    // ==== 틀린 코드 ====
+    // 로그 한 번 찍어보면 알겠지만 adapter가 계속 생기는 건 상식적으로 말이 안됨...
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+//        this.position = position;
+//        holder.setCalendarMonth(dateList.get(position).getMonth() + "월");
+//        gridAdapter = new GridAdapter(dateList.get(position).getDays(), (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE), mOnTextChangedListener);
+//        adapters.add(gridAdapter);
+//        Log.d("CustomCalendar", "grid adapter size :: " + adapters.size());
+//        holder.getCalendarGridView().setAdapter(gridAdapter);
     }
 
     @Override
