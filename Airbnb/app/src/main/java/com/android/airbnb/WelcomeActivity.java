62be37e755dac79843dac628ec1bc776ac764ab7
Daniel Min <dongyeon89@gmail.com>
2017-08-18 23:18:26 +0900

    페이스북 로그인 기능 추가 작업 완료

--- Airbnb/app/src/main/java/com/android/airbnb/WelcomeActivity.java
+++ Airbnb/app/src/main/java/com/android/airbnb/WelcomeActivity.java
@@ -17,14 +17,17 @@ import com.android.airbnb.main.GuestMainActivity;
 import com.android.airbnb.signup.SignUpActivity;
 import com.android.airbnb.util.PreferenceUtil;
 import com.android.airbnb.util.Remote.IServerApi;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
@@ -61,14 +64,33 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         btnLoginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(final LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+
                 GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
+
+                        String email = "";
 
                         Log.e("페이스북 로그인 결과 == ", response.toString());
                         Log.e("페이스북 토큰 : ", loginResult.getAccessToken().getToken());
                         Log.e("페이스북 User ID : ", loginResult.getAccessToken().getUserId());
                         Log.e("Facebook result : ",object.toString());
+
+                        try {
+                            email = object.getString("email");
+                            Log.e("페이스북 User Email : ", email);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        /*try {
+                            email = response.getJSONObject().getString("email").toString();
+                            Log.e("페이스북 User Email : ", email);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }*/
 
                         // 페이스북 Token SharedPreference에 저장
                         PreferenceUtil.setToken(WelcomeActivity.this, loginResult.getAccessToken().getToken());
@@ -120,13 +142,34 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                 Log.e("response.code()", response.code()+"");
                 Log.e("response.body() 1  :: ", response.body().toString());
 
-//                Log.e("response.body() 2  :: ", response.body().User);
-//                FacebookLoginResult.User user1 = new FacebookLoginResult.User();
 
 
-                //TODO - body 내부 안에 있는 객체의 값을 어떻게 꺼내야하는지? (FacebookLoginResult)
 
-                Log.e("response body Token ", "Success : " + response.body().getToken());
+
+                /*Log.e("User img_profile ", response.body().user.getImg_profile() );
+                Log.e("User first_name ", response.body().user.getFirst_name() );
+                Log.e("User last_name ", response.body().user.getLast_name() );
+                Log.e("User gender ", response.body().user.getGender() );
+                Log.e("User phone_num ", response.body().user.getPhone_num() );
+                Log.e("User birthday ", response.body().user.getBirthday() );
+                Log.e("User preference_lang ", response.body().user.getPreference_language() );
+                Log.e("User preference_curr ", response.body().user.getPreference_currency() );
+                Log.e("User living_site ", response.body().user.getLiving_site() );
+                Log.e("User introduce ", response.body().user.getIntroduce() );
+                Log.e("User last_login ", response.body().user.getLast_login() );
+                Log.e("User date_joined ", response.body().user.getDate_joined() );*/
+
+
+                Log.e("response User pk ", response.body().user.getPk() );
+                Log.e("response User username ", response.body().user.getUsername() );
+                Log.e("response User email ", response.body().user.getEmail() );
+                Log.e("response Token ", "Success : " + response.body().getToken());
+
+                // Shared Preference에 User Primary Key 저장
+                PreferenceUtil.setPrimaryKey(WelcomeActivity.this, response.body().user.getPk());
+                // Shared Preference에 User Email 저장
+                PreferenceUtil.setEmail(WelcomeActivity.this, response.body().user.getUsername());
+                // Shared Preference에 User Token 저장
                 PreferenceUtil.setToken(WelcomeActivity.this, response.body().getToken());
 
 
