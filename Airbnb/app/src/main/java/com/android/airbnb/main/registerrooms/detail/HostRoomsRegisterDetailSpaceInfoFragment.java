c5051774d3fe9f68f6b177c76e3e591d84b1e0d9
Daniel Min <dongyeon89@gmail.com>
2017-08-20 00:35:49 +0900

    HostRoomsRegisterDetailSpaceInfoFragment 뷰 및 코드 작성

--- Airbnb/app/src/main/java/com/android/airbnb/main/registerrooms/detail/HostRoomsRegisterDetailSpaceInfoFragment.java
+++ Airbnb/app/src/main/java/com/android/airbnb/main/registerrooms/detail/HostRoomsRegisterDetailSpaceInfoFragment.java
@@ -1,30 +1,79 @@
 package com.android.airbnb.main.registerrooms.detail;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.android.airbnb.R;
 
 /**
  * 숙소등록 2-4단계 (숙소내부소개 등록)
  */
-public class HostRoomsRegisterDetailSpaceInfoFragment extends Fragment {
+public class HostRoomsRegisterDetailSpaceInfoFragment extends Fragment implements View.OnClickListener{
 
+    private HostRoomsRegisterDetailActivity hostRoomsRegisterDetailActivity;
+    private EditText editTitle;
+    private ImageButton ImgBtnBack, ImgBtnNext;
+    private TextView txtTitle, txtLimit;
+    private View view = null;
 
     public HostRoomsRegisterDetailSpaceInfoFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        hostRoomsRegisterDetailActivity = (HostRoomsRegisterDetailActivity) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_host_rooms_register_detail_space_info, container, false);
+        if(view == null){
+            view = inflater.inflate(R.layout.fragment_host_rooms_register_detail_space_info, container, false);
+        }
+        setViews(view);
+        setListeners();
+        return view;
     }
 
+    private void setViews(View view) {
+        editTitle = (EditText) view.findViewById(R.id.editTitle);
+        txtLimit = (TextView) view.findViewById(R.id.txtLimit);
+        ImgBtnNext = (ImageButton) view.findViewById(R.id.ImgBtnNext);
+        ImgBtnBack = (ImageButton) view.findViewById(R.id.ImgBtnBack);
+        txtTitle = (TextView) view.findViewById(R.id.txtTitle);
+    }
+
+    private void setListeners(){
+        ImgBtnBack.setOnClickListener(this);
+        ImgBtnNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ImgBtnBack:
+                hostRoomsRegisterDetailActivity.onBackPressed();
+                break;
+            case R.id.ImgBtnNext:
+                goHostRoomsRegisterDetailGuestAccessFragment();
+                break;
+        }
+    }
+
+    private void goHostRoomsRegisterDetailGuestAccessFragment() {
+        hostRoomsRegisterDetailActivity.getSupportFragmentManager().beginTransaction()
+                .add(R.id.detailRoomsRegisterContainer, hostRoomsRegisterDetailActivity.hostRoomsRegisterDetailGuestAccessFragment)
+                .addToBackStack(null)
+                .commit();
+    }
 }
