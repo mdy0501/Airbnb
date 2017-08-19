31de4fbda7f0c9cf7501c0adab9f483bc97d194b
Daniel Min <dongyeon89@gmail.com>
2017-08-19 15:48:11 +0900

    HostRoomsRegisterDetailActivity 생성

---
+++ Airbnb/app/src/main/java/com/android/airbnb/main/registerrooms/detail/HostRoomsRegisterDetailActivity.java
@@ -0,0 +1,18 @@
+package com.android.airbnb.main.registerrooms.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.airbnb.R;
+
+/**
+ * 숙소등록 2단계 (상세정보)
+ */
+public class HostRoomsRegisterDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_rooms_register_detail);
+    }
+}
