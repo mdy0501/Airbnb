280ef7d87d029171c33529469cc2aa0d9d5e9355
Daniel Min <dongyeon89@gmail.com>
2017-08-21 23:34:47 +0900

    숙소등록간 - 사용자 사용공간 체크여부 코드 작성

--- Airbnb/app/src/main/java/com/android/airbnb/main/registerrooms/basic/HostRoomsRegisterBasicSpaceFragment.java
+++ Airbnb/app/src/main/java/com/android/airbnb/main/registerrooms/basic/HostRoomsRegisterBasicSpaceFragment.java
@@ -2,18 +2,19 @@ package com.android.airbnb.main.registerrooms.basic;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.android.airbnb.R;
-import com.android.airbnb.main.registerrooms.HostRoomsRegisterActivity;
+
+import static com.android.airbnb.main.registerrooms.HostRoomsRegisterActivity.hostingHouse;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +22,7 @@ import com.android.airbnb.main.registerrooms.HostRoomsRegisterActivity;
 public class HostRoomsRegisterBasicSpaceFragment extends Fragment implements View.OnClickListener {
 
     private HostRoomsRegisterBasicActivity hostRoomsRegisterBasicActivity;
-    private CheckBox checkBox1, checkBox3, checkBox5, checkBox7, checkBox8, checkBox9, checkBox11, checkBox12;
+    private CheckBox checkBox1, checkBox3, checkBox5, checkBox7, checkBox8, checkBox9, checkBox12;
     private ImageButton ImgBtnNext, ImgBtnBack;
     private TextView txtTitle, textView1, textView2, textView3, textView4, textView5, textView6, textView7, textView8, textView9, textView10, textView11, textView12, textView13;
     private View view = null;
@@ -45,6 +46,7 @@ public class HostRoomsRegisterBasicSpaceFragment extends Fragment implements Vie
         }
         setViews(view);
         setListeners();
+        setCheckBoxListeners();
         return view;
     }
 
@@ -62,11 +64,10 @@ public class HostRoomsRegisterBasicSpaceFragment extends Fragment implements Vie
         checkBox7 = (CheckBox) view.findViewById(R.id.checkBox7);
         textView8 = (TextView) view.findViewById(R.id.textView8);
         checkBox8 = (CheckBox) view.findViewById(R.id.checkBox8);
-        textView9 = (TextView) view.findViewById(R.id.detail_house_more_calendar);
+        textView9 = (TextView) view.findViewById(R.id.textView9);
         textView10 = (TextView) view.findViewById(R.id.textView10);
         checkBox9 = (CheckBox) view.findViewById(R.id.checkBox9);
         textView11 = (TextView) view.findViewById(R.id.textView11);
-        checkBox11 = (CheckBox) view.findViewById(R.id.checkBox11);
         textView12 = (TextView) view.findViewById(R.id.textView12);
         checkBox12 = (CheckBox) view.findViewById(R.id.checkBox12);
         textView13 = (TextView) view.findViewById(R.id.textView13);
@@ -87,9 +88,89 @@ public class HostRoomsRegisterBasicSpaceFragment extends Fragment implements Vie
                 hostRoomsRegisterBasicActivity.onBackPressed();
                 break;
             case R.id.ImgBtnNext:
-                Intent intent = new Intent(getActivity(), HostRoomsRegisterActivity.class);
-                startActivity(intent);
+                if(hostingHouse.facilities != null){
+                    hostingHouse.facilities = hostingHouse.facilities.substring(0, hostingHouse.facilities.length() - 2);
+                }
+                hostingHouse.setAmenities(hostingHouse.facilities);
+                getFragmentManager().beginTransaction().remove(getFragmentManager().findFragmentById(R.id.basicRoomsRegisterContainer)).commit();
+                getActivity().finish();
                 break;
         }
+    }
+
+    private void setCheckBoxListeners(){
+        if(hostingHouse.facilities != null){
+            hostingHouse.facilities = hostingHouse.facilities + ", ";
+        }
+
+        // 부엌
+        checkBox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    hostingHouse.facilities = hostingHouse.facilities + "Kitchen, ";
+                }
+            }
+        });
+
+        // 세탁 - 세탁기
+        checkBox3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    hostingHouse.facilities = hostingHouse.facilities + "Washer, ";
+                }
+            }
+        });
+
+        // 세탁 - 건조기
+        checkBox5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    hostingHouse.facilities = hostingHouse.facilities + "Dryer, ";
+                }
+            }
+        });
+
+        // 건물 내 무료 주차
+        checkBox7.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    hostingHouse.facilities = hostingHouse.facilities + "Free_parking, ";
+                }
+            }
+        });
+
+        // 엘리베이터
+        checkBox8.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    hostingHouse.facilities = hostingHouse.facilities + "Elevator, ";
+                }
+            }
+        });
+
+        // 수영장
+        checkBox9.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    hostingHouse.facilities = hostingHouse.facilities + "Pool, ";
+                }
+            }
+        });
+
+        // 헬스장
+        checkBox12.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    hostingHouse.facilities = hostingHouse.facilities + "Gym, ";
+                }
+            }
+        });
     }
 }
