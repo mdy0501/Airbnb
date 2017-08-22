ff6712da4d33b632f1a74458688ea4d2784afa25
JunHee Lee <jhlee910609@gmail.com>
2017-08-22 23:13:34 +0900

    Reservation step Activity -> fragment로 변경

--- Airbnb/app/src/main/java/com/android/airbnb/ReservationThreeFragment.java
+++ Airbnb/app/src/main/java/com/android/airbnb/ReservationThreeFragment.java
@@ -1,27 +1,35 @@
 package com.android.airbnb;
 
+
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.airbnb.calendar.Utils;
 import com.android.airbnb.domain.airbnb.House;
 import com.android.airbnb.domain.airbnb.House_images;
-import com.android.airbnb.domain.reservation.ReservationSingleTon;
+import com.android.airbnb.domain.reservation.Reservation;
 import com.android.airbnb.util.GlideApp;
 import com.android.airbnb.util.PreferenceUtil;
+import com.android.airbnb.util.Remote.ITask;
 import com.android.airbnb.util.Remote.Loader;
-import com.tsengvn.typekit.TypekitContextWrapper;
 
 import java.io.UnsupportedEncodingException;
 
-public class ReservationThreeActivity extends AppCompatActivity implements View.OnClickListener{
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ReservationThreeFragment extends Fragment implements View.OnClickListener, ITask.postReservation {
 
     private TextView reservationRoomType;
     private TextView reservationHostName;
@@ -36,26 +44,39 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
     private String checkOut = "";
     private Context context;
     private House house;
+    private DetailHouseActivity presenter;
+
+    public void setPresenter(DetailHouseActivity presenter) {
+        this.presenter = presenter;
+    }
+
+    public ReservationThreeFragment() {
+        // Required empty public constructor
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reservation_three);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_reservation_three, container, false);
         getData();
-        initView();
-        setData(ReservationSingleTon.getInstnace().getHouse());
+        initView(view);
+        setData(presenter.getReservation().getHouse());
+        return view;
     }
 
     private void setData(House house) {
         reservationRoomType.setText(house.getRoom_type());
         String cleaingFee = house.getCleaning_fee();
+
         reservationCalculateCleaningFee.setText(cleaingFee != null ? "₩" + cleaingFee : "청소비는 부과되지 않습니다.");
         reservationHostName.setText(house.getHost().getFirst_name() + ", " + house.getHost().getLast_name());
-        long period = Utils.CalendarUtil.calculatePeriod(checkIn, checkOut);
-        reservationRentalFee.setText("₩" + house.getPrice_per_day() + " X " + period +"박");
-        reservationTotalRentalFee.setText("₩"+calculateTotalRentalFee(period, house.getPrice_per_day()));
 
-        String[] total = {house.getPrice_per_day(), house.getCleaning_fee(), calculateTotalRentalFee(period, house.getPrice_per_day())};
+        long period = Utils.CalendarUtil.calculatePeriod(checkIn, checkOut);
+        reservationRentalFee.setText("₩" + house.getPrice_per_day() + " X " + period + "박");
+        reservationTotalRentalFee.setText("₩" + calculateTotalRentalFee(period, house.getPrice_per_day()));
+
+        String[] total = {house.getCleaning_fee(), calculateTotalRentalFee(period, house.getPrice_per_day())};
         reservationTotalFee.setText("₩" + calculateTotalFee(total));
 
         House_images[] houseImages = house.getHouse_images();
@@ -71,15 +92,15 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
 
     }
 
-    private void getData(){
-
-        checkIn = ReservationSingleTon.getInstnace().getCheckin_date();
-        checkOut = ReservationSingleTon.getInstnace().getCheckout_date();
-        house = ReservationSingleTon.getInstnace().getHouse();
+    private void getData() {
+        Reservation reservation = presenter.getReservation();
+        house = reservation.getHouse();
+        checkIn = reservation.getCheckin_date();
+        checkOut = reservation.getCheckout_date();
 
     }
 
-    private String calculateTotalRentalFee(long period, String pricePerDay){
+    private String calculateTotalRentalFee(long period, String pricePerDay) {
         return period * (Integer.parseInt(pricePerDay)) + "";
 
     }
@@ -88,29 +109,23 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
         int sum = 0;
         for (int i = 0; i < fee.length; i++) {
             int each = 0;
-            if (fee[i] != null && !fee[i].equals(""))
+            if (fee[i] != null || !fee[i].equals(""))
                 each = Integer.parseInt(fee[i]);
             sum += each;
         }
         return sum + "";
     }
 
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
-    }
-
-    private void initView() {
-        reservationRoomType = (TextView) findViewById(R.id.reservation_room_type);
-        reservationHostName = (TextView) findViewById(R.id.reservation_host_name);
-        reservationRoomImg = (ImageView) findViewById(R.id.reservation_room_img);
-        reservationRentalFee = (TextView) findViewById(R.id.reservation_rental_fee);
-        reservationTotalRentalFee = (TextView) findViewById(R.id.reservation_total_rental_fee);
-        reservationCalculateCleaningFee = (TextView) findViewById(R.id.reservation_calculate_cleaning_fee);
-        reservationTotalFee = (TextView) findViewById(R.id.reservation_total_fee);
-        btnCancel = (Button) findViewById(R.id.btn_cancel);
-        btnReserve = (Button) findViewById(R.id.btn_reserve);
+    private void initView(View view) {
+        reservationRoomType = (TextView) view.findViewById(R.id.reservation_room_type);
+        reservationHostName = (TextView) view.findViewById(R.id.reservation_host_name);
+        reservationRoomImg = (ImageView) view.findViewById(R.id.reservation_room_img);
+        reservationRentalFee = (TextView) view.findViewById(R.id.reservation_rental_fee);
+        reservationTotalRentalFee = (TextView) view.findViewById(R.id.reservation_total_rental_fee);
+        reservationCalculateCleaningFee = (TextView) view.findViewById(R.id.reservation_calculate_cleaning_fee);
+        reservationTotalFee = (TextView) view.findViewById(R.id.reservation_total_fee);
+        btnCancel = (Button) view.findViewById(R.id.btn_cancel);
+        btnReserve = (Button) view.findViewById(R.id.btn_reserve);
 
         btnCancel.setOnClickListener(this);
         btnReserve.setOnClickListener(this);
@@ -119,7 +134,7 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
     @Override
     public void onClick(View v) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(v.getContext());
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_cancel:
                 setCancelDialog(dialogBuilder);
                 break;
@@ -130,7 +145,7 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
         }
     }
 
-    private void setReserveDialog(AlertDialog.Builder builder){
+    private void setReserveDialog(AlertDialog.Builder builder) {
         builder.setTitle("예약 완료 확인");
         builder.setMessage("정말로 해당 House를 예약하시겠습니까?")
                 .setCancelable(true)
@@ -140,11 +155,11 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
                             public void onClick(DialogInterface dialog, int which) {
                                 // 예약 관련 엑티비티 한번에 꺼지게 하기
                                 try {
-                                    Loader.postReservation("Token " + PreferenceUtil.getToken(ReservationThreeActivity.this), house.getPk(), checkIn, checkOut);
+                                    Loader.postReservation("Token " + PreferenceUtil.getToken(presenter), house.getPk(), checkIn, checkOut, ReservationThreeFragment.this);
                                 } catch (UnsupportedEncodingException e) {
                                     e.printStackTrace();
                                 }
-                                ReservationThreeActivity.this.finish();
+                                presenter.removeAllFragments();
                             }
                         })
                 .setNegativeButton("아니요",
@@ -159,7 +174,7 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
         alertDialog.show();
     }
 
-    private void setCancelDialog(AlertDialog.Builder builder){
+    private void setCancelDialog(AlertDialog.Builder builder) {
         builder.setTitle("예약 취소");
         builder.setMessage("정말로 해당 House를 예약을 취소하시겠습니까?")
                 .setCancelable(true)
@@ -167,8 +182,7 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
                         new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                // 예약 관련 엑티비티 한번에 꺼지게 하기
-                                ReservationThreeActivity.this.finish();
+                                presenter.removeAllFragments();
                             }
                         })
                 .setNegativeButton("아니요",
@@ -181,5 +195,11 @@ public class ReservationThreeActivity extends AppCompatActivity implements View.
 
         AlertDialog alertDialog = builder.create();
         alertDialog.show();
+    }
+
+    @Override
+    public void getReservationResponse(String message) {
+        Toast.makeText(presenter, message, Toast.LENGTH_SHORT).show();
+        presenter.setReservation(null);
     }
 }
